94d8389e3d30df77a33aaa20dd58d32ce0bb80db
edward <damon_edward@qq.com>
2022-05-16 13:37:38 +0800

    update：update tencent info

--- organizations-common/src/main/java/com/lhamster/util/Constants.java
+++ organizations-common/src/main/java/com/lhamster/util/Constants.java
@@ -5,35 +5,35 @@ package com.lhamster.util;
  */
 public class Constants {
     /**
-     * 短信 appid
+     * 短信 appid（修改成你自己的）
      */
-    public static final String SMSAPPID = "1400396638";
+    public static final String SMSAPPID = "123456";
     /**
-     * 存储桶的名称
+     * 存储桶的名称（修改成你自己的）
      */
-    public static final String bucketName = "lhamster-organizations-1302533254";
+    public static final String bucketName = "lhamster-organizations-123456";
     /**
      * 腾讯云账户密钥对: secretId
      */
-    public static final String SMSSECREID = "AKIDrTqxoPq1ScXBZhbWPq22vJRNXZNvIVxo";
+    public static final String SMSSECREID = "SMSSECREID";
     /**
      * 腾讯云账户密钥对: secretKey
      */
-    public static final String SMSSECREKEY = "kocASnJBtNhFk4piMArmoZx8CWhbrVxO";
+    public static final String SMSSECREKEY = "SMSSECREKEY";
     /**
      * SmsSdkAppid
      */
-    public static final String SMSSDKAPPID = "1400396638";
+    public static final String SMSSDKAPPID = "123456";
     /**
      * sms sign   : sms签名
      */
-    public static final String SMSSIGN = "小仓鼠网";
+    public static final String SMSSIGN = "社团网站";
     /**
      * sms 注册短信模板id:
      */
-    public static final String SMSTEMPREGISTERLATEID = "659656";
+    public static final String SMSTEMPREGISTERLATEID = "123456";
     /**
      * sms 修改密码短信模板id:
      */
-    public static final String SMSTEMPCHANGEPWDLATEID = "659657";
+    public static final String SMSTEMPCHANGEPWDLATEID = "123456";
 }
